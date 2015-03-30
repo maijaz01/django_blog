bf87e195ee6c5a25c56b0e77e3db29f68db0a490
aijaz shekh <aiajzshekh0117@gmail.com>
2015-03-30 23:36:56 +0530

    urls are working as epected

--- blog/urls.py
+++ blog/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 import blog
 
+
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'blog.views.HomeMassage', name='Homepage'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^about/$', 'blog.views.about', name='about'),
-    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', 'blog.views.category', name='category'),) 
+    url(r'^category/(?P<category_name>[\w\-]+)/$', 'blog.views.category', name='category'),)
 
-    # url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls))
 
--- blog/views.py
+++ blog/views.py
@@ -5,37 +5,22 @@ from blog.models import Page
 
 # Create your views here.
 def HomeMassage(request):
-	import pdb;pdb.set_trace()
+	# import pdb;pdb.set_trace()
 	category_list = Category.objects.order_by('id')[1:]
 	context_dict = {'categories': category_list}
 	# context_dict = {'boldmessage': "I am bold font from the context"}
 	return render(request,'Home_Page.html' ,context_dict)
 
-def category(request, category_name_slug):
+def category(request, category_name):
+	context_dict = {}
+	# import pdb;pdb.set_trace()
+	category = Category.objects.get(name=category_name)
+	category_name=category.name
+	category_id=category.id
+	context_dict['category_name'] = category.name
+	pages = Page.objects.filter(category_id=category_id)
+	# Adds our results list to the template context under name pages.
+	context_dict['pages'] = pages
+	context_dict['category'] = category
 
-    # Create a context dictionary which we can pass to the template rendering engine.
-    context_dict = {}
-
-    try:
-        # Can we find a category name slug with the given name?
-        # If we can't, the .get() method raises a DoesNotExist exception.
-        # So the .get() method returns one model instance or raises an exception.
-        category = Category.objects.get(slug=category_name_slug)
-        context_dict['category_name'] = category.name
-
-        # Retrieve all of the associated pages.
-        # Note that filter returns >= 1 model instance.
-        pages = Page.objects.filter(category=category)
-
-        # Adds our results list to the template context under name pages.
-        context_dict['pages'] = pages
-        # We also add the category object from the database to the context dictionary.
-        # We'll use this in the template to verify that the category exists.
-        context_dict['category'] = category
-    except Category.DoesNotExist:
-        # We get here if we didn't find the specified category.
-        # Don't do anything - the template displays the "no category" message for us.
-        pass
-
-    # Go render the response and return it to the client.
-    return render(request, 'blog/category.html', context_dict)
+	return render(request, 'category.html', context_dict)
